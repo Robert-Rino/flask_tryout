0a0fc0ab9e4179b8f6624403edaf3bfa217efb0b
ninochang <nino.chang@swag.live>
2021-01-31 16:39:12 +0800

    Setup config

--- app/config.py
+++ app/config.py
@@ -1,13 +1,22 @@
 import os
+import datetime
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-dev.db')
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+    # flask-sql-alchemy
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or 'sqlite:///' + os.path.join(basedir, 'data-dev.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SECRET_KEY = 'development-key'
-    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+
+    # flask-jwt-exxtended
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY'] or 'secret'
+    JWT_IDENTITY_CLAIM = os.environ['JWT_IDENTITY_CLAIM'] or 'sub'
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=14)
+
 
     @staticmethod
     def init_app(app):
